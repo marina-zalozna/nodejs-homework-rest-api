07306f63f3c45af98c97f557627062415893fcc3
Marina Zalozna <marina.zalozna@gmail.com>
2023-03-26 13:25:30 +0200

    HW 04

--- controllers/contacts/add.js
+++ controllers/contacts/add.js
@@ -1,11 +1,13 @@
 const { Contact } = require('../../models/contact');
 
 const add = async (req, res) => {
-    const data = await Contact.create(req.body);
+    const { _id } = req.user;
+    
+    const result = await Contact.create({ ...req.body, owner: _id });
     res.status(201).json({ 
         status: "success", 
         code: 201,
-        data: { data } });
+        data: { result } });
 }
 
 module.exports = add;
